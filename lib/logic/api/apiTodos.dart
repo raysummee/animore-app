fdc0244639440fe508a1cb0c936439e6a73de797
Angshuman Barpujari <angshuarin@gmail.com>
2021-04-28 23:53:53 +0530

    replaced hardcoded petId in apiTodos

--- lib/logic/api/apiTodos.dart
+++ lib/logic/api/apiTodos.dart
@@ -1,12 +1,14 @@
 
 import 'package:animore/logic/api/authentication/auth.dart';
 import 'package:animore/logic/api/base/baseHttp.dart' as http;
+import 'package:animore/logic/helper/petHelper.dart';
 import 'package:animore/logic/helper/todosHelper.dart';
 
 import 'apiConfig.dart';
 class ApiTodos{
   Future<void> getTodosAllRequest() async{
-    const petId = 2;
+    //TODO replace index 0 with the index of pet which is currenty choosed
+    final petId = (await PetHelper().getPetAt(0)).id;
     var url = Uri.parse("$host/todos/$petId");
     http.get(
       url,
