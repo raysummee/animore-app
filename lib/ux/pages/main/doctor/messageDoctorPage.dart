5921f20803f8ea3714de96e11d0471a0c8b49c2f
Angshuman Barpujari <angshuarin@gmail.com>
2021-05-05 21:19:37 +0530

    chat message bubble ui build

--- lib/ux/pages/main/doctor/messageDoctorPage.dart
+++ lib/ux/pages/main/doctor/messageDoctorPage.dart
@@ -2,6 +2,7 @@ import 'package:animore/logic/model/modelVetBook.dart';
 import 'package:animore/ux/components/appbar/chatAppbar.dart';
 import 'package:animore/ux/components/group/chatInputGroup.dart';
 import 'package:animore/ux/components/others/customCircleAvatar.dart';
+import 'package:bubble/bubble.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -23,8 +24,15 @@ class MessageDoctorPage extends StatelessWidget {
                 image: DecorationImage(
                   fit: BoxFit.cover,
                   colorFilter: ColorFilter.mode(Colors.white.withOpacity(0.6), BlendMode.color),
-                  image: AssetImage("lib/assets/images/chatBGCyan.png")
+                  image: AssetImage("lib/assets/images/chatBGCyan.png"),
                 )
+              ),
+              child: ListView.builder(
+                padding: EdgeInsets.fromLTRB(22, 16, 22, 16),
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return index%2==0?recievedMessage("Hello"):sentMessage("Hello, back");
+                },
               ),
             ) 
           ),
@@ -33,4 +41,68 @@ class MessageDoctorPage extends StatelessWidget {
       )
     );
   }
+}
+
+Widget recievedMessage(String msg) {
+  return Bubble(
+    margin: BubbleEdges.only(top: 10),
+    padding: BubbleEdges.fromLTRB(16, 10, 16, 10),
+    alignment:Alignment.topLeft,
+    nip: BubbleNip.leftTop,
+    nipRadius: 0,
+    nipWidth: 1,
+    nipHeight: 20,
+    radius: Radius.circular(20),
+    child: Text(
+      msg.trim(), 
+      textAlign: TextAlign.right,
+      style: TextStyle(
+        fontSize: 16
+      ),
+    ),
+  );
+}
+
+Widget sentMessage(String msg){
+  return Bubble(
+    margin: BubbleEdges.only(top: 10),
+    padding: BubbleEdges.fromLTRB(16, 10, 16, 10),
+    alignment: Alignment.topRight,
+    nip: BubbleNip.rightTop,
+    nipRadius: 0,
+    nipWidth: 1,
+    nipHeight: 20,
+    radius: Radius.circular(20),
+    color: Color.fromRGBO(225, 255, 199, 1.0),
+    child: Text(
+      msg.trim(), 
+      textAlign: TextAlign.right,
+      style: TextStyle(
+        fontSize: 16
+      ),
+    ),
+  );
+}
+
+Widget dateBubble(DateTime date){ 
+  final now = DateTime.now();
+  final today = DateTime(now.year, now.month, now.day);
+  final yesterday = DateTime(now.year, now.month, now.day - 1);
+  final tomorrow = DateTime(now.year, now.month, now.day + 1);
+
+  final aDate = DateTime(date.year, date.month, date.day);
+
+  String dateString = DateFormat("dd MMM").format(date);
+  if(aDate == today) {
+    dateString = "today";
+  } else if(aDate == yesterday) {
+    dateString = "yesterday";
+  } else if(aDate == tomorrow) {
+    dateString = "tomorrow";
+  }
+  return Bubble(
+    alignment: Alignment.center,
+    color: Color.fromRGBO(212, 234, 244, 1.0),
+    child: Text(dateString.toUpperCase(), textAlign: TextAlign.center, style: TextStyle(fontSize: 11.0)),
+  );
 }
