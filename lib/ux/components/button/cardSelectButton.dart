b126f8ab6ea5e0232f16e007afa489a8ff4c4766
Angshuman Barpujari <angshuarin@gmail.com>
2021-05-12 01:26:16 +0530

    cardSelectButton fix

--- lib/ux/components/button/cardSelectButton.dart
+++ lib/ux/components/button/cardSelectButton.dart
@@ -1,8 +1,13 @@
+import 'package:animore/logic/api/apiImportantEvent.dart';
+import 'package:animore/logic/api/apiTodos.dart';
+import 'package:animore/logic/helper/petHelper.dart';
 import 'package:animore/logic/model/modelPet.dart';
 import 'package:animore/logic/provider/petSelectNotify.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:provider/provider.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class CardSelectButton extends StatelessWidget {
   final ModelPet pet;
@@ -24,35 +29,42 @@ class CardSelectButton extends StatelessWidget {
             subtitle: Text("${pet.type}: ${pet.bread}"),
             trailing: Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: AnimatedContainer(
-                  duration: Duration(milliseconds: 200),
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle, 
-                    border: Border.all(color: Colors.cyan),
-                    color: provider.index==index? Colors.cyan : Colors.white
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: provider.index==index
-                      ?Icon(
-                        Icons.check,
-                        size: 10.0,
-                        color: Colors.white,
-                      )
-                      :Container(
-                        width: 10,
-                        height: 10,
-                      )
-                    ),
-                  ),
+                child: ValueListenableBuilder(
+                  valueListenable: PetHelper().box().listenable(),
+                  builder: (context, Box<ModelPet> box, child) {
+                    return AnimatedContainer(
+                      duration: Duration(milliseconds: 200),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle, 
+                        border: Border.all(color: Colors.cyan),
+                        color: provider.id==pet.id? Colors.cyan : Colors.white
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: provider.id==pet.id
+                          ?Icon(
+                            Icons.check,
+                            size: 10.0,
+                            color: Colors.white,
+                          )
+                          :Container(
+                            width: 10,
+                            height: 10,
+                          )
+                        ),
+                      );
+                  }
+                ),
               )
           ),
         ),
         Material(
           color: Colors.transparent,
           child: InkWell(
-            onTap: (){
-              provider.index = index;
+            onTap: () async{
+              provider.id = pet.id;
+              await ApiTodos().all(id: pet.id);
+              await ApiImportantEvent().all(id: pet.id);
             },
             onLongPress: onLongPress,
             child: Container(
