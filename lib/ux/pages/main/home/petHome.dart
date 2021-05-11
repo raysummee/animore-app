51a8ba3af5950063db561b469c82d44967d0ada1
Angshuman Barpujari <angshuarin@gmail.com>
2021-05-11 15:31:05 +0530

    the pet box is now taken from petHelper of method selected pet synchronised

--- lib/ux/pages/main/home/petHome.dart
+++ lib/ux/pages/main/home/petHome.dart
@@ -2,6 +2,7 @@ import 'package:animated_size_and_fade/animated_size_and_fade.dart';
 import 'package:animore/logic/api/apiImportantEvent.dart';
 import 'package:animore/logic/api/apiPet.dart';
 import 'package:animore/logic/helper/importantEventHelper.dart';
+import 'package:animore/logic/helper/petHelper.dart';
 import 'package:animore/logic/model/modelPet.dart';
 import 'package:animore/logic/provider/petCardEditNotify.dart';
 import 'package:animore/ux/components/card/petCard.dart';
@@ -10,6 +11,8 @@ import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
+
+import '../../../../main.dart';
 
 class PetHome extends StatefulWidget {
   @override
@@ -29,12 +32,12 @@ class _PetHomeState extends State<PetHome> with TickerProviderStateMixin {
               vsync: this,
               child: provider.isEditing?
                 PetEditCard(
-                  box.get(0),
+                  PetHelper().selectedPet(navigatorKey.currentContext),
                   ImportantEventHelper().all(), 
                   0
                 ):
                 PetCard(
-                  "${box.get(0).name}",
+                  PetHelper().selectedPet(navigatorKey.currentContext).name,
                   ImportantEventHelper().recent()
                 ),
               fadeDuration: Duration(milliseconds: 300),
