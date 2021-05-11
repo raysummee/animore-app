18709190f0ef69b149b1280aabe88bc787f3afc2
Angshuman Barpujari <angshuarin@gmail.com>
2021-05-11 16:56:21 +0530

    all box open will be explicitly open by other method as openBox and box, and added closeAll method

--- lib/logic/helper/todosHelper.dart
+++ lib/logic/helper/todosHelper.dart
@@ -1,5 +1,7 @@
+import 'package:animore/logic/helper/petHelper.dart';
 import 'package:animore/logic/model/modelTodos.dart';
 import 'package:animore/logic/util/dateUtil.dart';
+import 'package:animore/main.dart';
 import 'package:hive/hive.dart';
 import 'package:intl/intl.dart';
 
@@ -7,32 +9,40 @@ class TodosHelper{
 
   Box<ModelTodos> today(){
     var weekName = DateUtil().todayWeekName();
-    return Hive.box<ModelTodos>("Todos_$weekName");
+    return box(weekName);
   }
 
   Future<Box<ModelTodos>> todayFuture() async{
     var weekName = DateUtil().todayWeekName();
-    return await Hive.openBox<ModelTodos>("Todos_$weekName");
+    return await openBox(weekName);
   }
 
   Future<Box<ModelTodos>> fromDateFuture(DateTime date) async{
     var weekName = DateUtil().weekName(date);
-    return await Hive.openBox<ModelTodos>("Todos_$weekName");
+    return await openBox(weekName);
   }
 
    Box<ModelTodos> fromWeek(String week){
     week = toBeginningOfSentenceCase(week);
-    return Hive.box<ModelTodos>("Todos_$week");
+    return box(week);
   }
 
   Future<void> closeFromDate(DateTime date) async{
     var weekName = DateUtil().weekName(date);
-    bool isOpen = Hive.isBoxOpen("Todos_$weekName");
+    bool isOpen = isBoxOpen(weekName);
     if(!isOpen) return;
-    await Hive.box<ModelTodos>("Todos_$weekName").close();
+    await box(weekName).close();
   }
 
-
+  Future<void> closeAll() async{
+    var weekNames = ["mon", "tue", "wed", "thu", "fri", "sat"];
+    weekNames.forEach((weekName) async{
+      bool isOpen = isBoxOpen(weekName);
+      if(!isOpen) return;
+      print("closing $weekName");
+      await box(weekName).close();
+    });
+  }
  
 
   Future<void> retainDefault() async{
@@ -40,36 +50,36 @@ class TodosHelper{
     weekNames.forEach((weekName) async{
       if(weekName==DateUtil().todayWeekName()) return;
       if(weekName==DateUtil().tomorrowWeekName()) return;
-      bool isOpen = Hive.isBoxOpen("Todos_$weekName");
+      bool isOpen = isBoxOpen(weekName);
       if(!isOpen) return;
       print("closing $weekName");
-      await Hive.box<ModelTodos>("Todos_$weekName").close();
+      await box(weekName).close();
     });
   }
 
   Future<Box<ModelTodos>> tomorrowFuture() async{
     var weekName = DateUtil().tomorrowWeekName();
-    return await Hive.openBox<ModelTodos>("Todos_$weekName");
+    return await openBox(weekName);
   }
  
   Future<void> edit(ModelTodos todos, String weekName) async{
-    Box<ModelTodos> box = await Hive.openBox<ModelTodos>("Todos_$weekName");
+    Box<ModelTodos> box = await openBox(weekName);
     await box.put(todos.id, todos);
   }
 
   Future<void> add(ModelTodos todos, String weekName) async{
-    Box<ModelTodos> box = await Hive.openBox<ModelTodos>("Todos_$weekName");
+    Box<ModelTodos> box = await openBox(weekName);
     await box.put(todos.id, todos);
   }
 
   Future<void> delete(ModelTodos todos, String weekName) async{
-    Box<ModelTodos> box = await Hive.openBox<ModelTodos>("Todos_$weekName");
+    Box<ModelTodos> box = await openBox(weekName);
     await box.delete(todos.id);
   }
 
 
   Future<void> _fetchTodo(Map<String, dynamic> body, String weekName) async{
-    Box<ModelTodos> box = await Hive.openBox<ModelTodos>("Todos_$weekName");
+    Box<ModelTodos> box = await openBox(weekName);
 
     if(body['todos'][weekName.toLowerCase()]==null||(body['todos'][weekName.toLowerCase()] as List).isEmpty) return;
 
@@ -98,6 +108,21 @@ class TodosHelper{
     await _fetchTodo(body, "Fri");
     await _fetchTodo(body, "Sat");
     await _fetchTodo(body, "Sun");
+  }
+
+  Future<Box<ModelTodos>> openBox(String weekName) async{
+    final petId = PetHelper().selectedPetAsync(navigatorKey.currentContext);
+    return await Hive.openBox<ModelTodos>("Todos_${weekName}_$petId");
+  }
+
+  Box<ModelTodos> box(String weekName) {
+    final petId = PetHelper().selectedPetAsync(navigatorKey.currentContext);
+    return Hive.box<ModelTodos>("Todos_${weekName}_$petId");
+  }
+ 
+  bool isBoxOpen(String weekName) {
+    final petId = PetHelper().selectedPetAsync(navigatorKey.currentContext);
+    return Hive.isBoxOpen("Todos_${weekName}_$petId");
   }
 
 }
