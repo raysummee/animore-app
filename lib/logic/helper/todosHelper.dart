5ff8308ce08efdb1ea2084f513bfd3ada5fb71a6
Angshuman Barpujari <angshuarin@gmail.com>
2021-05-13 23:18:32 +0530

    box is renamed as openBox and openBox is renamed as openBoxFuture

--- lib/logic/helper/todosHelper.dart
+++ lib/logic/helper/todosHelper.dart
@@ -9,29 +9,29 @@ class TodosHelper{
 
   Box<ModelTodos> today(){
     var weekName = DateUtil().todayWeekName();
-    return box(weekName);
+    return openBox(weekName);
   }
 
   Future<Box<ModelTodos>> todayFuture() async{
     var weekName = DateUtil().todayWeekName();
-    return await openBox(weekName);
+    return await openBoxFuture(weekName);
   }
 
   Future<Box<ModelTodos>> fromDateFuture(DateTime date) async{
     var weekName = DateUtil().weekName(date);
-    return await openBox(weekName);
+    return await openBoxFuture(weekName);
   }
 
    Box<ModelTodos> fromWeek(String week){
     week = toBeginningOfSentenceCase(week);
-    return box(week);
+    return openBox(week);
   }
 
   Future<void> closeFromDate(DateTime date) async{
     var weekName = DateUtil().weekName(date);
     bool isOpen = isBoxOpen(weekName);
     if(!isOpen) return;
-    await box(weekName).close();
+    await openBox(weekName).close();
   }
 
   Future<void> closeAll() async{
@@ -40,7 +40,7 @@ class TodosHelper{
       bool isOpen = isBoxOpen(weekName);
       if(!isOpen) return;
       print("closing $weekName");
-      await box(weekName).close();
+      await openBox(weekName).close();
     });
   }
  
@@ -53,22 +53,22 @@ class TodosHelper{
       bool isOpen = isBoxOpen(weekName);
       if(!isOpen) return;
       print("closing $weekName");
-      await box(weekName).close();
+      await openBox(weekName).close();
     });
   }
 
   Future<Box<ModelTodos>> tomorrowFuture() async{
     var weekName = DateUtil().tomorrowWeekName();
-    return await openBox(weekName);
+    return await openBoxFuture(weekName);
   }
  
   Future<void> edit(ModelTodos todos, String weekName) async{
-    Box<ModelTodos> box = await openBox(weekName);
+    Box<ModelTodos> box = await openBoxFuture(weekName);
     await box.put(todos.id, todos);
   }
 
   Future<void> add(ModelTodos todos, String weekName) async{
-    Box<ModelTodos> box = await openBox(weekName);
+    Box<ModelTodos> box = await openBoxFuture(weekName);
     await box.put(todos.id, todos);
   }
 
@@ -79,10 +79,11 @@ class TodosHelper{
 
 
   Future<void> _fetchTodo(Map<String, dynamic> body, String weekName) async{
-    Box<ModelTodos> box = await openBox(weekName);
+    Box<ModelTodos> box = await openBoxFuture(weekName);
 
     if(body['todos'][weekName.toLowerCase()]==null||(body['todos'][weekName.toLowerCase()] as List).isEmpty){
       await box.clear();
+      return;
     }
 
     List<ModelTodos> todos = (body['todos'][weekName.toLowerCase()] as List).map((e) => ModelTodos.fromMap(e)).toList();
@@ -112,12 +113,12 @@ class TodosHelper{
     await _fetchTodo(body, "Sun");
   }
 
-  Future<Box<ModelTodos>> openBox(String weekName) async{
+  Future<Box<ModelTodos>> openBoxFuture(String weekName) async{
     final petId = PetHelper().selectedId(navigatorKey.currentContext);
     return await Hive.openBox<ModelTodos>("Todos_${weekName}_$petId");
   }
 
-  Box<ModelTodos> box(String weekName) {
+  Box<ModelTodos> openBox(String weekName) {
     final petId = PetHelper().selectedId(navigatorKey.currentContext);
     return Hive.box<ModelTodos>("Todos_${weekName}_$petId");
   }
