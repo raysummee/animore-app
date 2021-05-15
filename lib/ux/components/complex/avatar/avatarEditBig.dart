a5b2fe1860278e2304c1690c0ffb0a0ae4bd1b09
Angshuman Barpujari <angshuarin@gmail.com>
2021-05-15 21:06:07 +0530

    avatarEditBig component is created.

---
+++ lib/ux/components/complex/avatar/avatarEditBig.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+
+class AvatarEditBig extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(15),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width-280,
+            height: MediaQuery.of(context).size.width-280,
+            child: Image.asset(
+              "lib/assets/images/dog_person.png",
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width-280,
+            height: MediaQuery.of(context).size.width-280,
+            decoration: BoxDecoration(
+              color: Colors.black.withOpacity(0.5),
+            ),
+          ),
+          Icon(
+            FlutterIcons.edit_2_fea, 
+            color: Colors.white,
+          ),
+          Material(
+            color: Colors.transparent,
+            child: InkWell(
+              splashColor: Colors.cyan.withOpacity(0.5),
+              onTap: (){},
+              child: Container(
+                width: MediaQuery.of(context).size.width-280,
+                height: MediaQuery.of(context).size.width-280,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
