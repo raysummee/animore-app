103f975fad77214833c386fc1535beefdbe4687d
Angshuman Barpujari <angshuarin@gmail.com>
2021-05-20 21:54:05 +0530

    added prod link

--- lib/logic/api/apiConfig.dart
+++ lib/logic/api/apiConfig.dart
@@ -2,6 +2,8 @@ String devHost = "http://127.0.0.1:8000";
 
 String stagingHost = "https://agile-mesa-13515.herokuapp.com";
 
+String prodHost = "https://animore-responsible-warthog-yw.eu-gb.mybluemix.net";
+
 
 String hostRoot = stagingHost;
 
