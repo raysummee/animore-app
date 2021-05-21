c90bba05040b8bdea808cbafbe0dc373d12e0809
Angshuman Barpujari <angshuarin@gmail.com>
2021-05-21 15:50:59 +0530

    the host for prod is slower than expected, revert back to heroku, which is much faster

--- lib/logic/api/apiConfig.dart
+++ lib/logic/api/apiConfig.dart
@@ -2,9 +2,9 @@ String devHost = "http://127.0.0.1:8000";
 
 String stagingHost = "https://agile-mesa-13515.herokuapp.com";
 
-String prodHost = "https://animore-responsible-warthog-yw.eu-gb.mybluemix.net";
+String staging2Host = "https://animore-responsible-warthog-yw.eu-gb.mybluemix.net";
 
 
-String hostRoot = prodHost;
+String hostRoot = stagingHost;
 
 String host = "$hostRoot/api";
